0ac630fbc1aeae23266f5e0e6b0f53899bf2f320
Joyce Liao <joyliao07@gmail.com>
2019-01-18 18:14:58 -0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ python3 manage.py makemigrations --noinput && \
 python3 manage.py migrate --noinput && \
 python3 manage.py collectstatic --noinput && \
 
-gunicorn kickstarter.wsgi:application -w 3 -b :8000
+# gunicorn kickstarter.wsgi:application -w 3 -b :8000
